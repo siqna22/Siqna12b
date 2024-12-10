f3821e7d54d6372638e1c6103d616a72ab091ec0
siqna22 <s.ivanova2020@pmg-pernik.com>
2024-12-10 18:10:48 +0200

    Dog6

---
+++ DogApp6/DogsWebApp/DogsWebApp/Models/Breed/BreedPairViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogsWebApp.Models.Breed
+{
+    public class BreedPairViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Breed")]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ DogApp6/DogsWebApp/DogsWebApp/Models/Dog/DogDeleteViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DogsWebApp.Models.Breed;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogsWebApp.Models.Dog
+{
+    public class DogDeleteViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [Display(Name = "Name")]
+        public string Name { get; set; } = null!;
+
+        [Range(0, 30, ErrorMessage = "Age must be between 0 and 30")]
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Display(Name = "Breed")]
+        public int BreedId { get; set; }
+        public virtual List<BreedPairViewModel> Breeds { get; set; } = new List<BreedPairViewModel>();
+
+        [Display(Name = "Dog Picture")]
+        public string? Picture { get; set; }
+    }
+}
